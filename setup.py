ab6b5d4221ffe1d36c6ceab2f0d0da5f90689675
Md Mijanur Rahman <md.rahman@fau.de>
2022-11-25 22:51:15 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#create setup.py
+from setuptools import setup, find_packages
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+setup(
+    name='snowflake',
+    version='1.0',
+    description='Snowflake',
+    author='Md Mijanur Rahman',
+    author_email='md.rahmdn@fau.de',
+    install_requires=required
+    )
+
+
+
+
